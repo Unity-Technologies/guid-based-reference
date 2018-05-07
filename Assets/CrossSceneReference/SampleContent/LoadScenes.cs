e38e257cb00bf809835e5c655da0602c5f0f601a
william-w-armstrong <williama@unity3d.com>
2018-05-07 16:27:08 -0700

    Add a little GUI
    Makes things a bit more discoverable

--- Assets/CrossSceneReference/SampleContent/LoadScenes.cs
+++ Assets/CrossSceneReference/SampleContent/LoadScenes.cs
@@ -31,4 +31,31 @@ public class LoadScenes : MonoBehaviour
             }
         }
 	}
+
+    // a little User Interface 
+    private void OnGUI()
+    {
+        // this code is a great example of how NOT to write Unity code
+
+        Vector2 padding = new Vector2(5.0f, 5.0f);
+        Vector2 buttonSize = new Vector2(150.0f, 50.0f);
+        Rect buttonPosition = new Rect(padding, buttonSize);
+
+        for ( int i = 0; i < scenes.Count; ++i )
+        {
+            // PER FRAME ALLOCATION HERE
+            string buttonText = scenes[i].name;
+            buttonText += scenes[i].shouldLoad ? " : Unload" : " : Load";
+
+            if (GUI.Button(buttonPosition, buttonText))
+            {
+                // UGH! Lists mean I can't modify scenes[i].shouldLoad directly! I guess make an awkward copy and then set =/
+                SceneInfo newInfo = scenes[i];
+                newInfo.shouldLoad = !newInfo.shouldLoad;
+                scenes[i] = newInfo;
+            }
+
+            buttonPosition.y += buttonSize.y + padding.y;
+        }
+    }
 }
